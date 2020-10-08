bd463d66cecaeb7910b8184d3318e23596b026e8
thehairy <71461991+thehairy@users.noreply.github.com>
2020-10-08 20:47:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ for (const file of commandFiles) {
 }
 
 client.on('ready', () => {
-  console.log('The Bot is ready to share some hair!');
+  console.log('The Bot is ready to share some hair! 🥞');
   client.user.setPresence({
     activity: {
       name: `${client.users.cache.filter(user => !user.bot).size} ${stringbucket.status}`,
