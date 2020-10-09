4e0015e6856de526347d551a84123bbed2f84f3a
thehairy <brokenempirebe@gmail.com>
2020-10-09 16:48:39 +0200

    test

--- commands/avatar.js
+++ commands/avatar.js
@@ -2,7 +2,7 @@ const { MessageEmbed } = require('discord.js');
 
 module.exports = {
 	name: 'avatar', 
-	description: 'Avatar-URL', 
+	description: 'Avatar-URL Embed', 
 	execute(message, args) { 		     
 		let embed = new MessageEmbed()
 			.setImage(message.author.displayAvatarURL());
