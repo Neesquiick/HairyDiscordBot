822d9d74bb16ac893459b372ee233ef3056a88cf
thehairy <71461991+thehairy@users.noreply.github.com>
2020-10-10 21:39:25 +0200

    Update queue.js

--- commands/queue.js
+++ commands/queue.js
@@ -6,9 +6,9 @@ module.exports = {
 		const serverQueue = message.client.queue.get(message.guild.id);
 		if (!serverQueue) return message.channel.send('There is nothing playing.');
 		return message.channel.send(`
-__**Song queue:**__
-${serverQueue.songs.map(song => `**-** ${song.title}`).join('\n')}
-**Now playing:** ${serverQueue.songs[0].title}
+			__**Song queue:**__
+			${serverQueue.songs.map(song => `**-** ${song.title}`).join('\n')}
+			**Now playing:** ${serverQueue.songs[0].title}
 		`);
 	}
 };
